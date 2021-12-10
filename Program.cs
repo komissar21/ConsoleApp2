e0bc4a209f4298f4c251d1f5dbca519fc06852ea
komissarovrodion1 <komissarovrodion@gmail.com>
2021-12-10 17:48:07 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApp2;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XMLGeneration XMLTables = new XMLGeneration();
+            XMLTables.generate();
+            XMLTables.write();
+            XMLTables.read();
+
+        }
+    }
+}
